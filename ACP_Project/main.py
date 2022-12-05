7ab65bd51d06039e8c523f4357fc5b2b57f7e5e1
JohannaCatilo <117686904+JohannaCatilo@users.noreply.github.com>
2022-12-05 21:33:39 +0800

    Update main.py

--- ACP_Project/main.py
+++ ACP_Project/main.py
@@ -51,4 +51,42 @@ while player_hp > 0 and enemy_hp > 0
         print (f"The enemy now has {enemy_hp} hp and {enemy_stamina} stamina")
         turn = 'Enemy'
 
-    
+    elif action == 2 and player_stamina >= 20:
+        player_burst_attack = player.burst_atk()
+        enemy_hp = enemy_hp - player_burst_attack
+        player_stamina -= 20
+        time.sleep(2)
+        print (f"\n{name} just did {player_burst_attack} damage!")
+        print (f"{name} now has {player_hp} hp and {player_stamina} stamina")
+        ime.sleep(2)
+        print (f"The enemy now has {enemy_hp} hp and {enemy_stamina} stamina")
+        turn = 'Enemy'
+        
+    elif action == 3 and player_stamina >= 15:
+        player_regenerate = player.heal()
+        player_hp += player_regenerate
+        player_stamina -= 15
+        time.sleep(2)
+        print (f"\n{name} regenerated {player_regenerate} hp")
+        print (f"{name} has {player_hp} hp and {player_stamina} stamina")
+        turn = 'Enemy'
+
+    elif action == 2 or action == 3 and player_stamina < 15:
+        print (f"\n{name} you have {player_hp} hp and {player_stamina} stamina")
+        print (f"\n{name} your stamina is too low, recharge by doing Normal Attack ")
+    else:
+        print ("Enter a correct Action")
+
+else:
+    if enemy_stamina >= 20:
+        enemy_burst_attack = enemy.burst_atk()
+        player_hp = player_hp - enemy_burst_attack
+        enemy_stamina -= 20
+        time.sleep(2)
+        print (f"\n The enemy just did {enemy_burst_attack} damage!")
+        print (f"{name} now has {player_hp} hp and {player_stamina} stamina")
+        time.sleep(2)
+        print (f"The enemy now has {enemy_hp} hp and {enemy_stamina} stamina")
+        turn = name
+
+
