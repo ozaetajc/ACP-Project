1e4e9ec3089ff45d158c88ffc5743f34a2fb51fc
ozaetajc <113275812+ozaetajc@users.noreply.github.com>
2022-12-05 15:02:17 +0800

    Update main.py

--- ACP_Project/main.py
+++ ACP_Project/main.py
@@ -36,7 +36,7 @@ def first_turn():
         return name
 turn = first_turn()
 
-while player_hp > 0 and anemy_hp > 0
+while player_hp > 0 and enemy_hp > 0
     print (f"\n{turn}'s turn")
     if turn != 'Enemy':
         action = int(input(f"{name}, What will you do:\n1) Normal Attack\n2) Burst Attack\n3) Regen\n"))
@@ -45,5 +45,10 @@ while player_hp > 0 and anemy_hp > 0
         enemy_hp = enemy_hp - player_normal_attack
         player_stamina += 10
         time.sleep(2)
+        print (f"\n{name} just did {player_normal_attack} damage!")
+        print (f"{name} now has {player_hp} hp and {player_stamina} stamina")
+        time.sleep(2)
+        print (f"The enemy now has {enemy_hp} hp and {enemy_stamina} stamina")
+        turn = 'Enemy'
 
     
